454a84da1ecf597e8f1d344e134d99698075a0f8
Willow Chargin <wchargin@gmail.com>
2019-09-07 13:40:54 -0700

    Parse object ID from current commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    let out = Command::new("git")
+        .args(&["rev-parse", "--verify", "HEAD^{commit}"])
+        .output()?;
+    if !out.status.success() {
+        eprintln!(
+            "Failed to parse commit: {:?}",
+            String::from_utf8_lossy(&out.stderr)
+        );
+        return Ok(());
+    }
+    let oid = parse_oid(out.stdout).expect("successful rev-parse should give valid hash");
+    println!("Head is at {:?}", oid);
+    Ok(())
+}
+
+fn parse_oid(stdout: Vec<u8>) -> Result<String, Vec<u8>> {
+    let mut raw = String::from_utf8(stdout).map_err(|e| e.into_bytes())?;
+    match raw.pop() {
+        Some('\n') => return Ok(raw),
+        Some(other) => raw.push(other),
+        None => (),
+    }
+    Err(raw.into_bytes())
 }
