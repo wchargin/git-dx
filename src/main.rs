620d53d8ff0d605f6841ac270f0e197cc4363f0b
Willow Chargin <wchargin@gmail.com>
2024-11-06 13:07:22 -0800

    When merging, add `:/` instead of `.`
    So that if you run `git dx` from a subdirectory, we can still
    incorporate changes that are not under that directory.

--- src/main.rs
+++ src/main.rs
@@ -208,7 +208,7 @@ fn integrate(
         .output()?;
     if !out.status.success() {
         // Assume that this is due to conflicts.
-        let out = &Command::new("git").args(&["add", "."]).output()?;
+        let out = &Command::new("git").args(&["add", ":/"]).output()?;
         err::from_git(out, || "failed to stage".to_string())?;
         let out = &Command::new("git")
             .args(&["commit", "--no-edit", "--no-verify"])
